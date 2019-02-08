5e033d99fa25198803909b8808dad18065445ed9
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-02-08 22:08:55 +0300

    Create Counter.jsx

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react';
+import ReactDom from 'react-dom';
+
+export default class Counter extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            counter: 1,
+        }
+    }
+
+    handlerCount = (event) => {
+        const addition = event.target.name === 'plus'? 1 : -1;
+
+        this.setState((prevState) => ({
+            counter: prevState.counter + addition,
+        }));
+    }
+    render() {
+        const {counter} = this.state;
+        return (
+            <div>
+                <h2>Counter</h2>
+                <button name="minus" onClick = {this.handlerCount}>-</button>
+                {counter}
+                <button name="plus" onClick = {this.handlerCount}>+</button>
+            </div>
+        );
+    }
+}
