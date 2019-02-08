decb35ad10ea532f0973e98ed23032bfe3266002
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-02-08 22:08:35 +0300

    Create CommentForm.jsx

---
+++ src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+import ReactDom from 'react-dom';
+
+export default class CommentForm extends PureComponent {
+    render() {
+        return (
+            <div>
+                <h2>Comments</h2>
+            </div>
+        );
+    }
+}
