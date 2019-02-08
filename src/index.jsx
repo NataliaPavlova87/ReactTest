ca076aac7d69cbe2e02f01fc21a72ddc10ac8e49
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-02-08 22:09:05 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React, { PureComponent } from 'react';
 import ReactDom from 'react-dom';
 
+import Container from 'components/Container';
+
 class App extends PureComponent {
     render() {
         return (
-            <div className="container">
-                <h1>Hello</h1>
-            </div>
+            <Container />
         );
     }
 }
