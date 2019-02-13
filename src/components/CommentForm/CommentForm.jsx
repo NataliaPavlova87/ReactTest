abb31a59141e9466f807512fd56d953004076b3f
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-02-13 16:02:22 +0300

    Update CommentForm.jsx

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,11 +1,41 @@
 import React, { PureComponent } from 'react';
 import ReactDom from 'react-dom';
+import PropTypes from 'prop-types';
 
 export default class CommentForm extends PureComponent {
+    static propTypes = {
+        onComments: PropTypes.func.isRequired,
+    }
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            author: '',
+            message: '',
+        }
+    }
+
+    handleFieldsChange = (event) => {
+        this.setState ({
+            [event.target.name]: event.target.value,
+        });
+    }
+
+    handleClick = () => {
+        const {onComments} = this.props;
+        onComments(this.state);
+        this.setState ({message: ''});
+    }
+
     render() {
+        const {author, message} = this.state;
+
         return (
             <div>
-                <h2>Comments</h2>
+                <input name="author" onChange={this.handleFieldsChange} value={author} placeholder="Author" type="text"/><br />
+                <textarea name="message" onChange={this.handleFieldsChange} value={message} placeholder="Your message"></textarea><br />
+                <button onClick ={this.handleClick}>Send</button>
             </div>
         );
     }
