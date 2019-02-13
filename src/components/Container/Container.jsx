42c36c7f4666589ca11c88e645e6048e9d024d81
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-02-13 16:32:29 +0300

    Update Container.jsx

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -3,12 +3,14 @@ import ReactDom from 'react-dom';
 
 import Counter from 'components/Counter';
 import CommentList from 'components/CommentList';
+import Toggle from 'components/Toggle';
 
 export default function Container(props) {
         return (
             <div>
                 <Counter />
                 <CommentList />
+                <Toggle />
             </div>
         );
 }
