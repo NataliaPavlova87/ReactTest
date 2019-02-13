eb9048388cd95a32214a6dd1488050477c56a7d7
NataliaPavlova87 <natalia_pavlova@bk.ru>
2019-02-13 16:02:25 +0300

    Update CommentList.jsx

--- src/components/CommentList/CommentList.jsx
+++ src/components/CommentList/CommentList.jsx
@@ -1,12 +1,34 @@
+import './CommentsList.css';
+
 import React, { PureComponent } from 'react';
 import ReactDom from 'react-dom';
 
 import CommentForm from 'components/CommentForm';
 
 export default class CommentList extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comments: [],
+        }
+    }
+
+    handleComments = (comments) => {
+        this.setState((prevState) => ({
+            comments: prevState.comments.concat([comments]),
+        }));
+    }
+
     render() {
+        const {comments} = this.state;
         return (
-            <CommentForm />
+            <div className = "comments">
+                <ul>
+                    {comments.map((comments, idx) => <li key={idx}>{comments.author}: {comments.message}</li>)}
+                </ul>
+                <CommentForm onComments = {this.handleComments} />
+            </div>
         );
     }
 }
